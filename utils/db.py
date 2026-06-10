16cf3917eed33cdad3a97b4b2ad2934d01f61c5b
Uzair-opsAI <uzair.saiyed786@gmail.com>
2026-06-10 10:21:31 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,1 +1,22 @@
-print("Database")
+import sqlite3
+
+def init_db():
+
+    conn = sqlite3.connect("tracker.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS assignments (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        employee_name TEXT,
+        project_code TEXT,
+        project_description TEXT,
+        priority TEXT,
+        senior_name TEXT,
+        status TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
