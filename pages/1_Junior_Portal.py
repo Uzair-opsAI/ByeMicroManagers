17de2859971a7904e1d580aaa5446fbf2ba64d8e
Uzair-opsAI <uzair.saiyed786@gmail.com>
2026-06-10 10:22:30 +0530

    Update 1_Junior_Portal.py

--- pages/1_Junior_Portal.py
+++ pages/1_Junior_Portal.py
@@ -1,1 +1,64 @@
-print("Junior Portal")
+import streamlit as st
+import sqlite3
+
+st.title("Junior Portal")
+
+employee_name = st.text_input(
+    "Employee Name"
+)
+
+project_code = st.text_input(
+    "Project Code"
+)
+
+project_description = st.text_area(
+    "Project Description"
+)
+
+priority = st.selectbox(
+    "Priority",
+    ["Low", "Medium", "High", "Critical"]
+)
+
+senior_name = st.selectbox(
+    "Senior Responsible",
+    [
+        "Senior A",
+        "Senior B",
+        "Senior C"
+    ]
+)
+
+if st.button("Submit Request"):
+
+    conn = sqlite3.connect("tracker.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO assignments
+    (
+        employee_name,
+        project_code,
+        project_description,
+        priority,
+        senior_name,
+        status
+    )
+    VALUES (?,?,?,?,?,?)
+    """,
+    (
+        employee_name,
+        project_code,
+        project_description,
+        priority,
+        senior_name,
+        "Pending"
+    ))
+
+    conn.commit()
+    conn.close()
+
+    st.success(
+        "Request Submitted Successfully"
+    )
