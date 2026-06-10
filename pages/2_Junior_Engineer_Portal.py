6797640acb525aaa9f4ac439c64e5380554ad531
Uzair-opsAI <uzair.saiyed786@gmail.com>
2026-06-10 12:45:56 +0530

    Update 2_Junior_Engineer_Portal.py

--- pages/2_Junior_Engineer_Portal.py
+++ pages/2_Junior_Engineer_Portal.py
@@ -1,1 +1,126 @@
+import streamlit as st
+import sqlite3
 
+st.title("Junior Engineer Portal")
+
+JUNIORS = [
+    "Nayaabshaad Ansari",
+    "Nandini Jadav",
+    "Parvej Meman",
+    "Chaitrang Prabhu"
+]
+
+SENIORS = [
+    "VineetKumar Sandil",
+    "Nilay Patel",
+    "Viral Shah",
+    "Sameer Jain",
+    "Chandresh Salakiya",
+    "Ramesh Prajapati",
+    "Hardik Gohil",
+    "Kamlesh Prajapati",
+    "Rintu Midday",
+    "Nakul Bhatt",
+    "Kaustubh Soman",
+    "Parth Shah"
+]
+
+employee_name = st.selectbox(
+    "Junior Engineer Name",
+    JUNIORS
+)
+
+work_allocation_status = st.selectbox(
+    "Work hours assigned?",
+    [
+        "Assigned",
+        "Not Assigned"
+    ]
+)
+
+project_code = st.text_input(
+    "Project Code"
+)
+
+task_name = st.text_area(
+    "Task Assigned"
+)
+
+priority = st.selectbox(
+    "Priority",
+    ["Low","Medium","High","Critical"]
+)
+
+senior_name = st.selectbox(
+    "Assigned By",
+    SENIORS
+)
+
+approx_duration = st.number_input(
+    "Approx Duration (Hours)",
+    min_value=1,
+    max_value=500,
+    value=8
+)
+
+start_date = st.date_input(
+    "Start Date"
+)
+
+end_date = st.date_input(
+    "End Date"
+)
+
+start_time = st.time_input(
+    "Start Time"
+)
+
+end_time = st.time_input(
+    "End Time"
+)
+
+if st.button("Submit Junior Assignment"):
+
+    conn = sqlite3.connect("tracker.db")
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    INSERT INTO assignments
+    (
+        employee_name,
+        employee_type,
+        work_hours_assigned,
+        project_code,
+        project_description,
+        priority,
+        senior_name,
+        approx_duration,
+        start_date,
+        end_date,
+        start_time,
+        end_time,
+        status
+    )
+    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)
+    """,
+    (
+        employee_name,
+        "Junior Engineer",
+        work_allocation_status,
+        project_code,
+        task_name,
+        priority,
+        senior_name,
+        approx_duration,
+        str(start_date),
+        str(end_date),
+        str(start_time),
+        str(end_time),
+        "Pending"
+    ))
+
+    conn.commit()
+    conn.close()
+
+    st.success("Assignment Submitted")
