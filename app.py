96c08fc18f025008f44eddd687ea5eb8d1e14ae6
Uzair-opsAI <uzair.saiyed786@gmail.com>
2026-06-10 10:18:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Main App")
+import streamlit as st
+
+st.set_page_config(
+    page_title="ByeMicroManagers",
+    page_icon="⚡",
+    layout="wide"
+)
+
+st.title("⚡ ByeMicroManagers")
+
+st.subheader("Electrical Engineering Resource Tracker")
+
+st.success("Application Successfully Deployed")
