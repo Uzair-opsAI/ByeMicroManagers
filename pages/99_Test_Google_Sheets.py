5418735193835cf7b20ccf7511fc43766e994a3d
Uzair-opsAI <uzair.saiyed786@gmail.com>
2026-06-11 16:24:39 +0530

    Update 99_Test_Google_Sheets.py

--- pages/99_Test_Google_Sheets.py
+++ pages/99_Test_Google_Sheets.py
@@ -1,15 +1,7 @@
 import streamlit as st
 from utils.google_sheet import sheet
 
-st.title("Google Sheets Debug")
+st.write("Worksheet Name:")
+st.write(sheet.title)
 
-records = sheet.get_all_records()
-
-st.write("Records:")
-st.write(records)
-
-st.write("Row 1:")
-st.write(sheet.row_values(1))
-
-st.write("All Values:")
 st.write(sheet.get_all_values())
