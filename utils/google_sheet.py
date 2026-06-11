9d8d34b928d0f279af8e74a41115c524fe61234e
Uzair-opsAI <uzair.saiyed786@gmail.com>
2026-06-11 15:13:27 +0530

    Create google_sheet.py

---
+++ utils/google_sheet.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import gspread
+
+from google.oauth2.service_account import Credentials
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=SCOPES
+)
+
+client = gspread.authorize(creds)
+
+sheet = client.open(
+    "Engineering Resource Tracker"
+).worksheet(
+    "Assignments"
+)
